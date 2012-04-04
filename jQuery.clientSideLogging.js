5e47018811c90479cd3986f7ecb11a7f5ea05c43
Rob Miller <rob@bigfish.co.uk>
2012-04-04 23:08:31 +0100

    Fallback for older browsers that don't implement JSON.stringify.

--- jQuery.clientSideLogging.js
+++ jQuery.clientSideLogging.js
@@ -73,4 +73,24 @@
 
 		return _info;
 	};
+
+	// Fallback for older browsers that don't implement JSON.stringify
+	JSON.stringify = JSON.stringify || function (obj) {
+		var t = typeof (obj);
+		if (t != "object" || obj === null) {
+			// simple data type
+			if (t == "string") obj = '"'+obj+'"';
+			return String(obj);
+		} else {
+			// recurse array or object
+			var n, v, json = [], arr = (obj && obj.constructor == Array);
+			for (n in obj) {
+				v = obj[n]; t = typeof(v);
+				if (t == "string") v = '"'+v+'"';
+				else if (t == "object" && v !== null) v = JSON.stringify(v);
+				json.push((arr ? "" : '"' + n + '":') + String(v));
+			}
+			return (arr ? "[" : "{") + String(json) + (arr ? "]" : "}");
+		}
+	};
 })(jQuery);
